6c57e12e562b3b9cfe993787453387480433dfe5
DeniseGarc <aliciadenise0623@gmail.com>
2025-03-31 14:28:16 -0700

    Commit inicial: creación del proyecto separado por capas

---
+++ PresentacionRestaurante/src/main/java/com/presentacionrestaurante/PresentacionRestaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.presentacionrestaurante;
+
+/**
+ *
+ * @author Alici
+ */
+public class PresentacionRestaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
