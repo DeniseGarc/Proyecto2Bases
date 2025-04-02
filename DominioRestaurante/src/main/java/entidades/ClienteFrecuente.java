56f22ab0fe302250b62cd96beb3e44ace6fec2b1
MarisolRuizP <marisol.ruiz253120@potros.itson.edu.mx>
2025-04-01 20:34:35 -0600

    Mapeado de cliente y cliente frecuente

---
+++ DominioRestaurante/src/main/java/entidades/ClienteFrecuente.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Maryr
+ */
+@Entity
+@Table(name = "clientesFrecuentes")
+public class ClienteFrecuente extends Cliente implements Serializable {
+
+    @Transient
+    private int puntosFidelidad;
+    
+    @Transient
+    private int cantidadVisitas;
+    
+    @Transient
+    private double gastoTotal;
+
+    public ClienteFrecuente() {
+    }
+
+    public ClienteFrecuente(String nombre, String apellidoPaterno, String apellidoMaterno, LocalDate fechaRegistro, String telefono, String correoElectronico) {
+        super(nombre, apellidoPaterno, apellidoMaterno, fechaRegistro, telefono, correoElectronico);
+    }
+    
+//    por mientras para ver como esta el rollo        
+//    public int calularPuntosFidelidad() {
+//        return puntosFidelidad;
+//    }
+//    
+//    public int calcularCantidadVisitas() {
+//        return cantidadVisitas;
+//    }
+//    
+//    public double calcularGastoTotal() {
+//        return gastoTotal;
+//    }
+
+    public int getPuntosFidelidad() {
+        return puntosFidelidad;
+    }
+
+    public void setPuntosFidelidad(int puntosFidelidad) {
+        this.puntosFidelidad = puntosFidelidad;
+    }
+
+    public int getCantidadVisitas() {
+        return cantidadVisitas;
+    }
+
+    public void setCantidadVisitas(int cantidadVisitas) {
+        this.cantidadVisitas = cantidadVisitas;
+    }
+
+    public double getGastoTotal() {
+        return gastoTotal;
+    }
+
+    public void setGastoTotal(double gastoTotal) {
+        this.gastoTotal = gastoTotal;
+    }
+    
+    
+}
