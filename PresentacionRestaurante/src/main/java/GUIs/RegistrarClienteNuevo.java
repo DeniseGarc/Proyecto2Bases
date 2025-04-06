f338acdbc5986f6d366faa2c792836c8b40d4361
MarisolRuizP <marisol.ruiz253120@potros.itson.edu.mx>
2025-04-06 10:09:19 -0600

    Modulo de cliente funcionando

--- PresentacionRestaurante/src/main/java/GUIs/RegistrarClienteNuevo.java
+++ PresentacionRestaurante/src/main/java/GUIs/RegistrarClienteNuevo.java
@@ -4,9 +4,15 @@
  */
 package GUIs;
 
+import DTOs.ClienteFrecuenteDTO;
 import control.CoordinadorAplicacion;
+import control.exception.CoordinadorException;
 import java.awt.Image;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,11 +21,13 @@ import javax.swing.ImageIcon;
 public class RegistrarClienteNuevo extends javax.swing.JFrame {
 
     private final CoordinadorAplicacion coordinador = new CoordinadorAplicacion();
+
     /**
      * Creates new form RegistrarClienteNuevo
      */
     public RegistrarClienteNuevo() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -184,43 +192,20 @@ public class RegistrarClienteNuevo extends javax.swing.JFrame {
     }//GEN-LAST:event_lblRegresarMouseClicked
 
     private void btnRegistrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRegistrarMouseClicked
-        
+        try {
+            ClienteFrecuenteDTO cliente = new ClienteFrecuenteDTO();
+            cliente.setNombre(txtNombre.getText().trim());
+            cliente.setFechaRegistro(Calendar.getInstance());
+            cliente.setTelefono(txtTelefono.getText().trim());
+            cliente.setCorreoElectronico(txtCorreo.getText().trim());
+            coordinador.registrarNuevoClienteFrecuente(cliente);
+            JOptionPane.showMessageDialog(this, "Cliente registrado exitosamente.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+            coordinador.PantallaClienteFrecuente(this);
+        } catch (CoordinadorException e) {
+            JOptionPane.showMessageDialog(this, "Error al registrar al cliente: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }//GEN-LAST:event_btnRegistrarMouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RegistrarClienteNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RegistrarClienteNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RegistrarClienteNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RegistrarClienteNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new RegistrarClienteNuevo().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnRegistrar;
