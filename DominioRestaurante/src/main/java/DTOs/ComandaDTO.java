05b0744e1e72b52b2e61ece472e2da6b548fcce7
erika <erika.vasquez253264@potros.itson.edu.mx>
2025-04-08 11:52:04 -0700

    comanda ya tiene mesa

--- DominioRestaurante/src/main/java/DTOs/ComandaDTO.java
+++ DominioRestaurante/src/main/java/DTOs/ComandaDTO.java
@@ -4,6 +4,7 @@
  */
 package DTOs;
 
+import entidades.Mesa;
 import enumeradores.Estado;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -20,6 +21,30 @@ public class ComandaDTO {
     private Estado estado;
     private List<DetalleComandaDTO> detallesComanda = new ArrayList<>();
     private String nombrecliente;
+    private Mesa mesa;
+
+    public ComandaDTO(Calendar fechaHora, double totalVenta, Estado estado, String nombrecliente, Mesa mesa) {
+        this.fechaHora = fechaHora;
+        this.totalVenta = totalVenta;
+        this.estado = estado;
+        this.nombrecliente = nombrecliente;
+        this.mesa = mesa;
+    }
+
+    public ComandaDTO(Calendar fechaHora, double totalVenta, Estado estado, Mesa mesa) {
+        this.fechaHora = fechaHora;
+        this.totalVenta = totalVenta;
+        this.estado = estado;
+        this.mesa = mesa;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
 
     public ComandaDTO() {
     }
