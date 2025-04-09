bd40348c1eb85bba72531f28fa793e8e45b45f4e
DeniseGarc <aliciadenise0623@gmail.com>
2025-04-09 00:40:26 -0700

    Imagenes en pantalla de inicio y principal
    Se le dio diseño bonito a las pantallas de inicio y de menu principal

--- PresentacionRestaurante/src/main/java/GUIs/MenuPrincipal.java
+++ PresentacionRestaurante/src/main/java/GUIs/MenuPrincipal.java
@@ -28,81 +28,94 @@ public class MenuPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lblTitulo = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        lblTitulo = new javax.swing.JLabel();
         btnComandas = new javax.swing.JButton();
         btnReportes = new javax.swing.JButton();
         btnMesas = new javax.swing.JButton();
         btnIngredientes = new javax.swing.JButton();
         btnProductos = new javax.swing.JButton();
         btnClientesFrecuentes = new javax.swing.JButton();
+        lblBtnIngredientes = new javax.swing.JLabel();
+        lblBtnMesas = new javax.swing.JLabel();
+        lblBtnReportes = new javax.swing.JLabel();
+        lblBtnComandas = new javax.swing.JLabel();
+        lblBtnClientes = new javax.swing.JLabel();
+        lblBtnProductos = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(1070, 700));
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        lblTitulo.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36)); // NOI18N
+        lblTitulo.setForeground(new java.awt.Color(255, 255, 255));
+        lblTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitulo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/icono-logo.png"))); // NOI18N
+        getContentPane().add(lblTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 10, 240, -1));
+
         jPanel1.setBackground(new java.awt.Color(255, 254, 245));
         jPanel1.setPreferredSize(new java.awt.Dimension(1070, 700));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel2.setBackground(new java.awt.Color(255, 176, 217));
         jPanel2.setPreferredSize(new java.awt.Dimension(1070, 78));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        lblTitulo.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 36)); // NOI18N
-        lblTitulo.setForeground(new java.awt.Color(255, 255, 255));
-        lblTitulo.setText("Hello Kitty Cafe");
-        jPanel2.add(lblTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 20, -1, 40));
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         btnComandas.setBackground(new java.awt.Color(254, 255, 203));
         btnComandas.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
-        btnComandas.setText("Comandas");
-        btnComandas.setPreferredSize(new java.awt.Dimension(150, 150));
+        btnComandas.setMaximumSize(new java.awt.Dimension(240, 220));
+        btnComandas.setMinimumSize(new java.awt.Dimension(240, 220));
+        btnComandas.setPreferredSize(new java.awt.Dimension(240, 220));
         btnComandas.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 btnComandasMouseClicked(evt);
             }
         });
+        jPanel1.add(btnComandas, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 160, 240, 220));
 
         btnReportes.setBackground(new java.awt.Color(255, 140, 200));
         btnReportes.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
         btnReportes.setForeground(new java.awt.Color(255, 255, 255));
-        btnReportes.setText("Reportes");
         btnReportes.setPreferredSize(new java.awt.Dimension(150, 150));
+        jPanel1.add(btnReportes, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 160, 240, 220));
 
         btnMesas.setBackground(new java.awt.Color(201, 220, 249));
-        btnMesas.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
+        btnMesas.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 24)); // NOI18N
         btnMesas.setForeground(new java.awt.Color(255, 255, 255));
-        btnMesas.setText("Mesas");
-        btnMesas.setPreferredSize(new java.awt.Dimension(150, 150));
+        btnMesas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/mesas-icon.png"))); // NOI18N
+        btnMesas.setMaximumSize(new java.awt.Dimension(240, 220));
+        btnMesas.setMinimumSize(new java.awt.Dimension(240, 220));
+        btnMesas.setPreferredSize(new java.awt.Dimension(240, 220));
+        jPanel1.add(btnMesas, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 160, 240, 220));
 
         btnIngredientes.setBackground(new java.awt.Color(255, 178, 217));
         btnIngredientes.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
         btnIngredientes.setForeground(new java.awt.Color(255, 255, 255));
-        btnIngredientes.setText("Ingredientes");
         btnIngredientes.setPreferredSize(new java.awt.Dimension(150, 150));
         btnIngredientes.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 btnIngredientesMouseClicked(evt);
             }
         });
+        jPanel1.add(btnIngredientes, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 420, 240, 220));
 
         btnProductos.setBackground(new java.awt.Color(255, 178, 217));
         btnProductos.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
         btnProductos.setForeground(new java.awt.Color(255, 255, 255));
-        btnProductos.setText("Productos");
         btnProductos.setPreferredSize(new java.awt.Dimension(150, 150));
         btnProductos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnProductosActionPerformed(evt);
             }
         });
+        jPanel1.add(btnProductos, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 420, 240, 220));
 
         btnClientesFrecuentes.setBackground(new java.awt.Color(255, 178, 217));
         btnClientesFrecuentes.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
         btnClientesFrecuentes.setForeground(new java.awt.Color(255, 255, 255));
-        btnClientesFrecuentes.setText("Clientes Frecuentes");
         btnClientesFrecuentes.setPreferredSize(new java.awt.Dimension(150, 150));
         btnClientesFrecuentes.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -114,55 +127,43 @@ public class MenuPrincipal extends javax.swing.JFrame {
                 btnClientesFrecuentesActionPerformed(evt);
             }
         });
+        jPanel1.add(btnClientesFrecuentes, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 420, 240, 220));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(160, 160, 160)
-                        .addComponent(btnIngredientes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnProductos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnMesas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(129, 129, 129)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnComandas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnClientesFrecuentes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(187, 187, 187))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addGap(161, 161, 161)
-                    .addComponent(btnReportes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(759, Short.MAX_VALUE)))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 86, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnComandas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnMesas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(46, 46, 46)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnIngredientes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnProductos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnClientesFrecuentes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(190, 190, 190))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addGap(171, 171, 171)
-                    .addComponent(btnReportes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(379, Short.MAX_VALUE)))
-        );
+        lblBtnIngredientes.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 24)); // NOI18N
+        lblBtnIngredientes.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBtnIngredientes.setText("Ingredientes");
+        lblBtnIngredientes.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(lblBtnIngredientes, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 640, 240, -1));
+
+        lblBtnMesas.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 24)); // NOI18N
+        lblBtnMesas.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBtnMesas.setText("Generar Mesas");
+        lblBtnMesas.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(lblBtnMesas, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 380, 240, -1));
+
+        lblBtnReportes.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 24)); // NOI18N
+        lblBtnReportes.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBtnReportes.setText("Reportes");
+        lblBtnReportes.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(lblBtnReportes, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 380, 240, -1));
+
+        lblBtnComandas.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 24)); // NOI18N
+        lblBtnComandas.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBtnComandas.setText("Comandas");
+        lblBtnComandas.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(lblBtnComandas, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 380, 240, -1));
+
+        lblBtnClientes.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 24)); // NOI18N
+        lblBtnClientes.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBtnClientes.setText("Clientes Frecuentes");
+        lblBtnClientes.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(lblBtnClientes, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 640, 240, -1));
+
+        lblBtnProductos.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 24)); // NOI18N
+        lblBtnProductos.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBtnProductos.setText("Productos");
+        lblBtnProductos.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(lblBtnProductos, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 640, 240, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
@@ -201,6 +202,12 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton btnReportes;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lblBtnClientes;
+    private javax.swing.JLabel lblBtnComandas;
+    private javax.swing.JLabel lblBtnIngredientes;
+    private javax.swing.JLabel lblBtnMesas;
+    private javax.swing.JLabel lblBtnProductos;
+    private javax.swing.JLabel lblBtnReportes;
     private javax.swing.JLabel lblTitulo;
     // End of variables declaration//GEN-END:variables
 }
